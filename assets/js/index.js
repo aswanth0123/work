12b63f8572c9a540f8d4a3b42d36219892bbae94
Vysakh <vysakh7995@gmail.com>
2025-03-26 10:31:37 +0530

    fixes

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function displayNav() {
+    document.getElementById('threedots').style.display = 'none';
+    document.getElementById('twolines').style.display = "flex";
+}
+
+function removeNav() {
+    document.getElementById('threedots').style.display = 'block';
+    document.getElementById('twolines').style.display = "none";
+}
