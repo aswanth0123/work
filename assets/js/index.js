1c50dbf11723ea5901a2cca6fee61fbb84b0a275
aswanth0123 <aswanthsuresh74@gmail.com>
2025-03-26 17:56:53 +0530

    video added

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,17 @@ function removeNav() {
     document.getElementById('threedots').style.display = 'block';
     document.getElementById('twolines').style.display = "none";
 }
+
+const scroller = document.querySelector('.scroller');
+let scrollAmount = 0;
+
+function scrollImages() {
+    scrollAmount -= 1; // Adjust speed
+    if (scrollAmount <= -scroller.offsetWidth / 2) {
+        scrollAmount = 0; // Reset to create infinite loop
+    }
+    scroller.style.transform = `translateX(${scrollAmount}px)`;
+    requestAnimationFrame(scrollImages);
+}
+
+scrollImages();
